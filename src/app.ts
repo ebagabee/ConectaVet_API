d8b740e2b4a5145de9803d21b7f8cb1419949ae0
ebagabee <alvesantos.gh@gmail.com>
2026-05-19 21:34:34 -0300

    chore: add instanced

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express from "express";
+import 'dotenv/config';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/hello", (_req, res) => {
-  res.json({ message: "Hello from ConectaVet API!" });
+app.get('/api/hello', (_req, res) => {
+  res.json({ message: 'Hello from ConectaVet API!' });
 });
+
+app.use('/api', routes);
 
 export default app;
