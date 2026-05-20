d109c69b186d3497ce6dfd04590a21a616aacd84
ebagabee <alvesantos.gh@gmail.com>
2026-05-19 21:48:51 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors({ origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000' }));
 app.use(express.json());
 
 app.get('/api/hello', (_req, res) => {
