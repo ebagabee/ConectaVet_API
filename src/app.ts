911f4b908a17baff3e3c6958798862eff85044f5
ebagabee <alvesantos.gh@gmail.com>
2026-05-19 21:19:23 -0300

    feat: add api

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/hello', (_req, res) => {
-  res.json({ message: 'Hello from ConectaVet API!' });
+app.get("/api/hello", (_req, res) => {
+  res.json({ message: "Hello from ConectaVet API!" });
 });
 
 export default app;
